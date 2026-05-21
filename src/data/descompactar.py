1dbc1752d9f60d25f0a3f23ff4e15ba2204f3b8e
ramoneirao <ramonneirao@gmail.com>
2026-05-21 20:41:10 -0300

    refactor: encapsula lógica de extração em uma função

--- src/data/descompactar.py
+++ src/data/descompactar.py
@@ -1,29 +1,22 @@
-# Descompactar o Dataset
-import sys
 import zipfile
-from pathlib import Path
-
-# Adiciona a pasta "src" ao sys.path 
-# O linter/IDE (Pylance/Pyright) por padrão entende que "src" é a raiz dos imports
-src_path = Path(__file__).resolve().parents[1]
-sys.path.append(str(src_path))
-
-from utils.logger import get_logger
+from src.utils.logger import get_logger
 
 logger = get_logger(__name__)
 
-arquivo = "data/raw/archive.zip"
-destino = "data/interim"
+def extrair_dados(arquivo: str = "data/raw/archive.zip", destino: str = "data/interim"):
+    """
+    Função responsável por extrair o dataset zipado para a pasta interim.
+    """
+    logger.info(f"Iniciando a extração do arquivo {arquivo}...")
 
-logger.info(f"Iniciando a extração do arquivo {arquivo}...")
+    try:
+        with zipfile.ZipFile(arquivo, "r") as zip_ref:
+            zip_ref.extractall(destino)
+            logger.info(f"Arquivos extraídos com sucesso para {destino}!")
+    except FileNotFoundError:
+        logger.error(f"Arquivo não encontrado: {arquivo}")
+    except Exception as e:
+        logger.error(f"Ocorreu um erro durante a extração: {e}")
 
-try:
-    with zipfile.ZipFile(arquivo, "r") as zip_ref:
-        zip_ref.extractall(destino)
-        logger.info(f"Arquivos extraídos com sucesso para {destino}!")
-except FileNotFoundError:
-    logger.error(f"Arquivo não encontrado: {arquivo}")
-except Exception as e:
-    logger.error(f"Ocorreu um erro durante a extração: {e}")
-
-    
+if __name__ == "__main__":
+    extrair_dados()
